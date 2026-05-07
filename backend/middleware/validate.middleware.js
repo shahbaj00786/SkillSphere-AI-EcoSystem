7db2cb7b48997003be04ca385423071b5b157b5d
Shahbaj Hussain <shahbajhussain@gmail.com>
2026-05-07 17:06:54 +0530

    middlware validator

---
+++ backend/middleware/validate.middleware.js
@@ -0,0 +1,17 @@
+const validate = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+      const messages = error.details.map((err) => err.message).join(', ');
+      return res.json({
+        success: false,
+        message: messages,
+      });
+    }
+
+    next();
+  };
+};
+
+export default validate;
