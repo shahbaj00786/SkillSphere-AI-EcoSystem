9e00b6b2dea448c5f140f42c51f2dc82c7739b6b
Shahbaj Hussain <shahbajhussain@gmail.com>
2026-05-07 16:57:09 +0530

    jwt token verify

---
+++ backend/utils/jwt.utils.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import env from '../config/env.js';
+
+const generateAccessToken = (userId, role) => {
+  return jwt.sign({ id: userId, role }, env.jwt.accessSecret, {
+    expiresIn: env.jwt.accessExpiry,
+  });
+};
+
+const generateRefreshToken = (userId) => {
+  return jwt.sign({ id: userId }, env.jwt.refreshSecret, {
+    expiresIn: env.jwt.refreshExpiry,
+  });
+};
+
+const verifyAccessToken = (token) => {
+  return jwt.verify(token, env.jwt.accessSecret);
+};
+
+const verifyRefreshToken = (token) => {
+  return jwt.verify(token, env.jwt.refreshSecret);
+};
+
+export {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+};
