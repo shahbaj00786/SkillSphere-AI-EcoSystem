c256a7e8457262aae6fad9de5b43b054b5846133
Shahbaj Hussain <shahbajhussain@gmail.com>
2026-05-07 16:50:52 +0530

    database tested

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,7 @@ const test = async () => {
   await connectDB();
 
   const TestModel = mongoose.model('Test', new mongoose.Schema({ message: String }));
-  await TestModel.create({ message: 'SkillSphere DB is working!' });
 
-  console.log('✅ Test data inserted successfully!');
   process.exit(0);
 };
 
