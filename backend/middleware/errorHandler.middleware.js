b21a1abb34497af61347a854c4b95ebad5ae43bf
Shahbaj Hussain <shahbajhussain231@gmail.com>
2026-05-08 09:15:58 +0530

    error handler updated

--- backend/middleware/errorHandler.middleware.js
+++ backend/middleware/errorHandler.middleware.js
@@ -1,8 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err.message);
+console.log(err.message);
 
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
 
   res.status(statusCode).json({
     success: false,
