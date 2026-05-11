0a0b06182a47b0bea31071a2b7a54800941b3a34
Shahbaj Hussain <shahbajhussain231@gmail.com>
2026-05-11 14:13:42 +0530

    all image added

---
+++ frontend/src/src/constants/images.js
@@ -0,0 +1,24 @@
+// Auth images
+export const LOGO = 'https://img.icons8.com/color/96/000000/briefcase.png';
+export const LOGIN_IMG = 'https://img.icons8.com/color/200/000000/login-rounded.png';
+export const REGISTER_IMG = 'https://img.icons8.com/color/200/000000/add-user-male.png';
+export const FORGOT_IMG = 'https://img.icons8.com/color/200/000000/lock.png';
+export const RESET_IMG = 'https://img.icons8.com/color/200/000000/unlock.png';
+export const VERIFY_IMG = 'https://img.icons8.com/color/200/000000/email-open.png';
+
+// Dashboard images
+export const DASHBOARD_IMG = 'https://img.icons8.com/color/200/000000/dashboard.png';
+export const FREELANCER_IMG = 'https://img.icons8.com/color/200/000000/worker-male.png';
+export const CLIENT_IMG = 'https://img.icons8.com/color/200/000000/businessman.png';
+export const ADMIN_IMG = 'https://img.icons8.com/color/200/000000/admin-settings-male.png';
+
+// Profile images
+export const PROFILE_IMG = 'https://img.icons8.com/color/200/000000/user-male-circle.png';
+export const PORTFOLIO_IMG = 'https://img.icons8.com/color/200/000000/portfolio.png';
+export const RESUME_IMG = 'https://img.icons8.com/color/200/000000/resume.png';
+
+// Default avatar
+export const DEFAULT_AVATAR = 'https://img.icons8.com/color/96/000000/user-male-circle--v1.png';
+
+// Empty states
+export const EMPTY_IMG = 'https://img.icons8.com/color/200/000000/nothing-found.png';
