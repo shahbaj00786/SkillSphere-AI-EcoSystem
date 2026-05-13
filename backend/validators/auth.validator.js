1cd46b8a95714f88ba7a2a0ffb0afc6320ea7667
Shahbaj Hussain <shahbajhussain231@gmail.com>
2026-05-13 21:24:35 +0530

    updated

--- backend/validators/auth.validator.js
+++ backend/validators/auth.validator.js
@@ -7,7 +7,7 @@ const registerSchema = Joi.object({
   }),
 
   email: Joi.string().email().required().messages({
-    'string.empty': 'Email is required',
+    'string.empty': 'Email is required.',
     'string.email': 'Please enter a valid email',
   }),
 
